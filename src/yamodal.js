ed313becd77129cc849afc85f20025e3dccadc70
romellem <romellem.git@gmail.com>
2020-05-20 08:47:29 -0500

    Adjusts comments on params

--- src/yamodal.js
+++ src/yamodal.js
@@ -11,10 +11,10 @@ import delegate from 'delegate';
  * @param {String} [opt.trigger_selector] Selector of the element(s) that when clicked, open our modal. Defaults to '[data-modal-trigger="${template.name}"]' or '[data-modal-trigger]' if template is an anonymous function.
  * @param {String} [opt.close_selector] Selector of the element(s) that when clicked, close its open modal. Defaults to '[data-modal-close]'.
  * @param {Function} [opt.onAppend] Optional function to append our modal to the DOM. Called with two arguments: `modal_node` and `trigger_node`. Defaults to `document.body.appendChild(modal_node)`.
- * @param {Function} [opt.beforeInsertIntoDom] Optional function that runs before inserting the modal into the DOM. Called with two arguments: `modal_node` and `trigger_node`.
+ * @param {Function} [opt.beforeInsertIntoDom] Optional function that runs before inserting the modal into the DOM. Called with three arguments: `modal_node`, `trigger_node`, and `event`. If this function returns `false`, modal will _not_ be injected and we bail early.
  * @param {Function} [opt.afterInsertIntoDom] Optional function that runs before inserting the modal into the DOM. Called with two arguments: `modal_node` and `trigger_node`. For instance, you can use this to fade in a modal.
- * @param {Boolean} [opt.removeModalAfterTransition] When false, element is removed immediates from DOM. Otherwise, element is removed after a 'transitionend' event has fired on the `modal_node`. Defaults to false.
- * @param {Function} [opt.beforeRemoveFromDom] Optional function that runs before removing the modal from the DOM. Called with two arguments: `modal_node` and `close_node`.
+ * @param {Boolean} [opt.removeModalAfterTransition] When false, element is removed immediately from DOM. Otherwise, element is removed after a 'transitionend' event has fired on the `modal_node`. Defaults to false.
+ * @param {Function} [opt.beforeRemoveFromDom] Optional function that runs before removing the modal from the DOM. Called with three arguments: `modal_node`, `close_node` and `event`. If this function returns `false`, modal will _not_ be removed and we bail early.
  * @param {Function} [opt.afterRemoveFromDom] Optional function that runs after removing the modal from the DOM. Called with two arguments: `modal_node` and `close_node`.
  * @param {Function} [opt.onAfterSetup] Optional function that runs once after all event listeners have been setup. Called with `modal_node` and an object with `open`, `close`, and `destroy` methods.
  * @param {Function} [opt.onDestroy] Optional function that runs additional cleanup steps if we "destroy" our listeners. Called with `modal_node`.
