2ad00a4e5c3b46d6c225b47f3063189316d889b4
romellem <romellem.git@gmail.com>
2020-05-20 10:59:54 -0500

    Changes default for close selector

--- src/yamodal.js
+++ src/yamodal.js
@@ -9,7 +9,7 @@ import delegate from 'delegate';
  * @param {Function} opt.template The main template function that returns a string of HTML. Called with the passed in `context`. The return value of the template should be a single HTML node.
  * @param {Any|Function} [opt.context] Optional context object to be passed into our template template. If a function is passed, it should return a plain object to be used as the context.
  * @param {String} [opt.trigger_selector] Selector of the element(s) that when clicked, open our modal. Defaults to '[data-modal-trigger="${template.name}"]' or '[data-modal-trigger]' if template is an anonymous function.
- * @param {String} [opt.close_selector] Selector of the element(s) that when clicked, close its open modal. Defaults to '[data-modal-close]'.
+ * @param {String} [opt.close_selector] Selector of the element(s) that when clicked, close its open modal. Defaults to '[data-modal-close]'. If the modal does not contain a an element matching `[data-modal-close]` then the modal itself will close when clicked.
  * @param {Function} [opt.onAppend] Optional function to append our modal to the DOM. Called with two arguments: `modal_node` and `trigger_node`. Defaults to `document.body.appendChild(modal_node)`.
  * @param {Function} [opt.beforeInsertIntoDom] Optional function that runs before inserting the modal into the DOM. Called with three arguments: `modal_node`, `trigger_node`, and `event`. If this function returns `false`, modal will _not_ be injected and we bail early.
  * @param {Function} [opt.afterInsertIntoDom] Optional function that runs before inserting the modal into the DOM. Called with three arguments: `modal_node`, `trigger_node`, and `event`.
@@ -25,7 +25,7 @@ const initializeModalListener = ({
 	template,
 	context,
 	trigger_selector,
-	close_selector = '[data-modal-close]',
+	close_selector,
 	onAppend,
 	afterInsertIntoDom,
 	beforeInsertIntoDom,
@@ -72,6 +72,18 @@ const initializeModalListener = ({
 		throw new Error('"template" must return a DOM node.');
 	}
 
+	/**
+	 * If no `close_selector` was passed, check if `[data-modal-close]` matches an
+	 * element within our modal. If not, then reset our close selector which in turn
+	 * means that the modal will be closed when itself is clicked.
+	 */
+	if (close_selector === undefined) {
+		close_selector = '[data-modal-close]';
+		if (!modal_node.querySelector(close_selector)) {
+			close_selector = undefined;
+		}
+	}
+
 	let isOpen = false;
 
 	const onTriggerOpen = function onTriggerOpen(event) {
@@ -91,6 +103,15 @@ const initializeModalListener = ({
 			if (bail_check === false) {
 				return;
 			}
+		}
+
+		/**
+		 * If no close_selector is set, then add a click listener to the modal that closes itself.
+		 * Note that this only happens if the default `[data-modal-close]` is not found
+		 * within the modal itself.
+		 */
+		if (close_selector === undefined) {
+			once(modal_node, 'click', onTriggerClose);
 		}
 
 		// Default is to just append the child to the <body>
@@ -142,7 +163,10 @@ const initializeModalListener = ({
 	};
 
 	const trigger_delegation = delegate(trigger_selector, 'click', onTriggerOpen);
-	let close_delegation = delegate(close_selector, 'click', onTriggerClose);
+	let close_delegation;
+	if (close_selector !== undefined) {
+		close_delegation = delegate(close_selector, 'click', onTriggerClose);
+	}
 
 	const rtn_object = {
 		// Use getter in case a dynamic context was passed
