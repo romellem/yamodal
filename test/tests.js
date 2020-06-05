e3bcdd1a4429ff9a27d57385001e7d6384b28e5f
romellem <romellem.git@gmail.com>
2020-06-05 11:33:04 -0500

    Working initial tests
    I _think_ right now we are creating a plain, empty page before each
    `it()` call. The HTML for that is:
    
        <!doctype html><html><head><meta charset="utf-8"></head><body></body></html>
    
    As set in global-jsdom - https://github.com/modosc/global-jsdom/blob/e5ef993/src/index.js

--- test/tests.js
+++ test/tests.js
@@ -1,51 +1,31 @@
-const { JSDOM } = require('jsdom');
+const yamodal = require('../dist/umd/yamodal');
 const assert = require('assert');
-
-const HTML = `<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-	
-</body>
-</html>`;
-
-const createDomAndExposeToGlobal = (html) => {
-	const dom = new JSDOM(html);
-	global.window = dom.window;
-	global.document = dom.document;
-	return dom;
-};
+const jsdom = require('global-jsdom');
 
 describe('yamodal', function () {
+	beforeEach(function () {
+		this.cleanup = jsdom();
+	});
+
+	afterEach(function () {
+		this.cleanup();
+	});
+
 	describe('errors', function () {
-		// let dom;
-		// beforeEach(function () {
-		// 	dom = createDomAndExposeToGlobal(HTML);
-		// });
+		it('should throw when no template is given', function () {
+			assert.throws(() => yamodal());
+		});
 
-		// it('should throw when no template is given', function () {
-		// 	assert.throws(() => yamodal());
-		// });
-
-		// it('should throw when template is not a function', function () {
-		// 	assert.throws(() => yamodal({ template: null }));
-		// 	assert.throws(() => yamodal({ template: true }));
-		// 	assert.throws(() => yamodal({ template: 1 }));
-		// 	assert.throws(() => yamodal({ template: '<div></div>' }));
-		// 	assert.throws(() => yamodal({ template: { html: '<div></div>' } }));
-		// });
+		it('should throw when template is not a function', function () {
+			assert.throws(() => yamodal({ template: null }));
+			assert.throws(() => yamodal({ template: true }));
+			assert.throws(() => yamodal({ template: 1 }));
+			assert.throws(() => yamodal({ template: '<div></div>' }));
+			assert.throws(() => yamodal({ template: { html: '<div></div>' } }));
+		});
 
 		it('should throw when template does not return a DOM node', function () {
-			// assert.throws(() => yamodal({ template: () => 'modal' }));
-			const dom = new JSDOM(HTML);
-	global.window = dom.window;
-	global.document = dom.document;
-
-	const yamodal = require('../dist/umd/yamodal.js');
-	global.yamodal = yamodal;
-			yamodal({ template: () => '<div>hello</div>' });
-			// assert.throws(() => yamodal({ template: () => '<div>hello</div>' }));
+			assert.throws(() => yamodal({ template: () => 'modal' }));
 		});
 	});
 });
