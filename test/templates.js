5f58b91b654b426682b8e59a36efcabb62d1e11b
romellem <romellem.git@gmail.com>
2020-08-22 13:21:10 -0500

    Changes imports to ESM from CJS

--- test/templates.js
+++ test/templates.js
@@ -1,8 +1,3 @@
-const basic = () => `<div id="modal">Hello</div>`;
-const basicWithClose = (close_btn = `<button data-modal-close="">x</button>`) =>
+export const basic = () => `<div id="modal">Hello</div>`;
+export const basicWithClose = (close_btn = `<button data-modal-close="">x</button>`) =>
 	`<div id="modal">Hello${close_btn}</div>`;
-
-module.exports = {
-	basic,
-	basicWithClose,
-};
